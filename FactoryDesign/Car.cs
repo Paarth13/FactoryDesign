5edbfeecaedcbc9a46f1615d863a26f0a732f0ba
Paarth Vashisht <pvashisht@tavisca.com>
2018-09-05 14:07:46 +0530

    Factory design implemented only

---
+++ FactoryDesign/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesign
+{
+    class Car :IProduct
+    {
+        public string TypeOfProduct()
+        {
+            return "CAr";
+        }
+
+        public void Save()
+        {
+            
+        }
+
+
+        public void Book()
+        {
+            Console.WriteLine("IN car booking");
+        }
+    }
+}
